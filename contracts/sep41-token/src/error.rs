265423b082bdcc1631d137c41d28f6431b3a6668
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-05-23 22:44:55 +0300

    feat: add contract error codes

---
+++ contracts/sep41-token/src/error.rs
@@ -0,0 +1,10 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum ContractError {
+    InsufficientFunds = 1,
+    InsufficientAllowance = 2,
+    NotAdmin = 3,
+    AllowanceExpired = 4,
+}
